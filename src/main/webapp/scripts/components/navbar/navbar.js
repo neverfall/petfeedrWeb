e8425c0d08857006f29397cafa0dbf0837860af8
NEVERFALL <carlosj_chacon@hotmail.es>
2015-07-16 11:23:26 -0600

    navbar mejorada

---
+++ src/main/webapp/scripts/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    // Sidebar
+    $(".button-collapse").sideNav(hover: false);
+    // Dropdown
+    $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false // Displays dropdown below the button
+        }
+    );
+});
